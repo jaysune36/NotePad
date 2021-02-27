a3c997694ec46d94d40fff444746b3b23e628668
user <user@users-MacBook-Pro.local>
2021-02-26 21:12:34 -0800

    was able to create function to have the notepad value added to the console, but if I move the notePadvalue variable outside of the eventscope, a blank log shows. will work on fixing this issue

--- Main.js
+++ Main.js
@@ -1,7 +1,11 @@
+let createList = document.createElement("li")
+let noteList = document.getElementById("noteList");
+let notePad = document.getElementById("notePad");
+let notePadValue = document.getElementById("notePad").value;
+let noteSave = document.getElementById("noteSave");
 
-document.getElementById("notePad").addEventListener("click", function() {
+noteSave.addEventListener("click", function() {
+  
+  console.log(notePadValue)
 
-})
-
-document.getElementById("noteSave").addEventListener("click", function() {
 })
