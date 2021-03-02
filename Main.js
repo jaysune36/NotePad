99c48be64632ec6edfeb0e68a4ce33916a457bf2
user <user@users-MacBook-Pro.local>
2021-03-01 21:13:51 -0800

    created function that will create a new li element with its value in the innterHTML and classname. The classname will be stamped with the date and time to help with future coding to identify specifically which element this refers too.

--- Main.js
+++ Main.js
@@ -1,11 +1,18 @@
-let createList = document.createElement("li")
-let noteList = document.getElementById("noteList");
-let notePad = document.getElementById("notePad");
-let notePadValue = document.getElementById("notePad").value;
+
 let noteSave = document.getElementById("noteSave");
 
 noteSave.addEventListener("click", function() {
-  
-  console.log(notePadValue)
 
+  let notePad = document.getElementById("notePad");
+  let notePadValue = document.getElementById("notePad").value;
+  let createList = document.createElement("li");
+  let noteList = document.getElementById("noteList");
+  let getNoteList = notePadValue + " [" + new Date() + "]";
+
+    noteList.insertAdjacentElement('afterbegin', createList);
+    createList.id = "noteListItem";
+    createList.className = getNoteList;
+    createList.innerHTML = notePadValue;
+    notePad.value = "";
+ 
 })
