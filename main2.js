c68ae9a5438f415f8d4d5150cb78a491b18c33cf
user <user@users-MacBook-Pro.local>
2021-05-12 10:03:16 -0700

    function created in getstorage to retreive key and add to class name for <li> so can easier delete individual items when page reloads. Also working on responsive design for notePad

--- main2.js
+++ main2.js
@@ -1,9 +1,9 @@
 // Created a 2nd js file, to better write my previous code witin the main.js file
 const notePad = document.querySelector('#notePad');
 const inputBtnContainer = document.querySelector('#inputBtnContainer');
-const noInput = document.querySelector('#noInput');
-const noteListContainer = document.querySelector('#noteContainer');
-const noteList = document.querySelector('#noteList');
+const noInput = document.querySelector('.noInput');
+const noteListContainer = document.querySelector('#noteListContainer');
+const noteList = document.querySelector('.noteList');
 
 function attachListItemBtn(li) {
   let deleteBtn = document.createElement('button');
@@ -16,28 +16,29 @@ function attachListItemBtn(li) {
   li.appendChild(editBtn);
 }
 
-function createList(li) {
-  let newItem = document.createElement('li');
-  newItem.className = Date.now();
-  newItem.textContent = li;
-  localStorage.setItem(newItem.className, li)
-  attachListItemBtn(newItem);
-  noteList.appendChild(newItem);
+function createNoteList(li) {
+  let newListItem = document.createElement('li');
+  newListItem.textContent = li;
+  newListItem.className = Date.now()
+  localStorage.setItem(newListItem.className, li);
+  attachListItemBtn(newListItem);
+  noteList.insertAdjacentElement("afterbegin", newListItem);
 }
-
-// function setStorage(liClassName, li) {
-//   localStorage.setItem(liClassName, li);
-// }
 
 function getStorage() {
   for(let i = 0; i < localStorage.length; i++) {
-    let getKey = localStorage.key(i);
-    let getItem = localStorage.getItem(getKey);
-    createList(getItem);
+    const getKey = localStorage.key(i);
+    const getItem = localStorage.getItem(getKey);
+    let newListItem = document.createElement('li');
+    newListItem.className = getKey;
+    newListItem.textContent = getItem;
+    attachListItemBtn(newListItem);
+    noteList.appendChild(newListItem);
     if(localStorage) {
       noInput.style.display = 'none';
-    }
+    } 
   }
+  
 }
 
 window.addEventListener('load', () => {
@@ -46,19 +47,19 @@ window.addEventListener('load', () => {
 
 inputBtnContainer.addEventListener('click', (e) => {
   if(e.target.tagName == 'BUTTON'){
-    if(e.target.id == 'noteSave') {
+    if(e.target.className == 'noteSave') {
         if(notePad.value) {
           noInput.style.display = 'none';
-          createList(notePad.value);
-          // setStorage(notePad.value);
+          createNoteList(notePad.value);
+          notePad.value = null;
         }
     }
 
-    if(e.target.id == 'clearInput') {
+    if(e.target.className == 'clearInput') {
       notePad.value = '';
     }
 
-    if(e.target.id == 'deleteAllInputs') {
+    if(e.target.className == 'deleteAllInputs') {
       noteList.innerHTML = '';
       noInput.style.display = 'block';
       localStorage.clear();
@@ -74,15 +75,16 @@ noteList.addEventListener('click', (e) => {
       let ul = li.parentNode;
       ul.removeChild(li);
       localStorage.removeItem(liClass);
-      alert(liClass)
       if(noteList.innerHTML === '') {
         noInput.style.display = 'block';
       }
     }
   } if(e.target.className == 'edit') {
     let li = e.target.parentNode;
+    let liClass = li.className;
     let ul = li.parentNode;
     ul.removeChild(li);
+    localStorage.removeItem(liClass);
     notePad.value = li.textContent.replace('DeleteEdit', '');
     if(noteList.innerHTML === '') {
       noInput.style.display = 'block';
