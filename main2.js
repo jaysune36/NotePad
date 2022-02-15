32bd27cfee7144f823b8d0472847edf3a315a833
user <user@users-MacBook-Pro.local>
2022-02-15 11:49:46 -0800

    changed createNote and buttonAttach function, workingo changing edit/delete eventlisteners

--- main2.js
+++ main2.js
@@ -5,28 +5,34 @@ const noInput = document.querySelector('.noInput');
 const noteListContainer = document.querySelector('#noteListContainer');
 const noteList = document.querySelector('.noteList');
 
-function attachListItemBtn(li) {
-  let deleteBtn = document.createElement('button');
-  deleteBtn.className = 'delete'
-  deleteBtn.textContent = 'Delete';
-  li.appendChild(deleteBtn)
-  let editBtn = document.createElement('button');
-  editBtn.className = 'edit';
-  editBtn.textContent = 'Edit';
-  li.appendChild(editBtn);
+function attachListItemBtn() {
+  let createDiv = document.createElement('div');
+  createDiv.className = 'btnDiv';
+  createDiv.innerHTML = `
+    <button class='delete'>Delete</button>
+    <button class='edit'>Edit</button>
+  `;
+  return createDiv;
 }
 
-function createNoteList(li) {
-  let newListItem = document.createElement('li');
-  newListItem.textContent = li;
-  newListItem.className = Date.now()
-  localStorage.setItem(newListItem.className, li);
-  attachListItemBtn(newListItem);
-  noteList.insertAdjacentElement("afterbegin", newListItem);
+function createStorage(li) {
+  const storageObj = {key: li.className, value: li}
+  localStorage.setItem(JSON.stringify(storageObj.key), JSON.stringify(storageObj.value))
 }
+
+function createNote(noteInfo) {
+  let createLi = document.createElement('li');
+  createLi.innerHTML = `
+    <span>${noteInfo}</span>
+  `;
+  createStorage(noteInfo);
+  createLi.appendChild(attachListItemBtn());
+  noteList.insertAdjacentElement('afterbegin', createLi);
+}
+
 
 function getStorage() {
-  for(let i = 0; i < localStorage.length; i++) {
+  for (let i = 0; i < localStorage.length; i++) {
     const getKey = localStorage.key(i);
     const getItem = localStorage.getItem(getKey);
     let newListItem = document.createElement('li');
@@ -34,11 +40,11 @@ function getStorage() {
     newListItem.textContent = getItem;
     attachListItemBtn(newListItem);
     noteList.appendChild(newListItem);
-    if(localStorage) {
+    if (localStorage) {
       noInput.style.display = 'none';
-    } 
+    }
   }
-  
+
 }
 
 window.addEventListener('load', () => {
@@ -46,20 +52,20 @@ window.addEventListener('load', () => {
 })
 
 inputBtnContainer.addEventListener('click', (e) => {
-  if(e.target.tagName == 'BUTTON'){
-    if(e.target.className == 'noteSave') {
-        if(notePad.value) {
-          noInput.style.display = 'none';
-          createNoteList(notePad.value);
-          notePad.value = null;
-        }
+  if (e.target.tagName == 'BUTTON') {
+    if (e.target.className == 'noteSave') {
+      if (notePad.value) {
+        noInput.style.display = 'none';
+        createNote(notePad.value);
+        notePad.value = null;
+      }
     }
 
-    if(e.target.className == 'clearInput') {
+    if (e.target.className == 'clearInput') {
       notePad.value = '';
     }
 
-    if(e.target.className == 'deleteAllInputs') {
+    if (e.target.className == 'deleteAllInputs') {
       noteList.innerHTML = '';
       noInput.style.display = 'block';
       localStorage.clear();
@@ -68,26 +74,38 @@ inputBtnContainer.addEventListener('click', (e) => {
 })
 
 noteList.addEventListener('click', (e) => {
-  if(e.target.tagName == 'BUTTON') {
-    if(e.target.className == 'delete') {
+  if (e.target.tagName == 'BUTTON') {
+    if (e.target.className == 'delete') {
       let li = e.target.parentNode;
       let liClass = li.className;
       let ul = li.parentNode;
       ul.removeChild(li);
       localStorage.removeItem(liClass);
-      if(noteList.innerHTML === '') {
+      if (noteList.innerHTML === '') {
         noInput.style.display = 'block';
       }
     }
-  } if(e.target.className == 'edit') {
-    let li = e.target.parentNode;
-    let liClass = li.className;
-    let ul = li.parentNode;
-    ul.removeChild(li);
-    localStorage.removeItem(liClass);
-    notePad.value = li.textContent.replace('DeleteEdit', '');
-    if(noteList.innerHTML === '') {
-      noInput.style.display = 'block';
-    }
+  } if (e.target.className == 'edit') {
+
+    // let li = e.target.parentNode;
+    // let liClass = li.className;
+    // let ul = li.parentNode;
+    // ul.removeChild(li);
+    // localStorage.removeItem(liClass);
+    // notePad.value = li.textContent.replace('DeleteEdit', '');
+    // if (noteList.innerHTML === '') {
+    //   noInput.style.display = 'block';
+    // }
   }
+
+  if (e.target.tagName == 'LI' && e.target.style.overflow == 'hidden') {
+    let liTarget = e.target;
+    liTarget.style.overflow = '';
+    liTarget.style.height = 'auto';
+  } else {
+    let liTarget = e.target
+    liTarget.style.overflow = 'hidden';
+    liTarget.style.height = '15vh';
+  }
+
 })
